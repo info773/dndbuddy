778b2ceac2147a1d19f3ab5986b75476c10f98a5
“Berry” <info@felix-behrens.de>
2026-03-05 23:15:57 +0100

    UI

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,11 @@ function Footer() {
   return (
     <div className="flex items-center gap-3 bg-slate-500 p-1">
       <p>&copy; Felix Behrens {new Date().getFullYear()}</p>
-      <img className="opacity-70 h-4" src="/logo_unicorn.png" alt="" />
+      <img
+        className="opacity-70 h-4 hover:rotate-1800 transition-transform duration-3000"
+        src="/logo_unicorn.png"
+        alt=""
+      />
     </div>
   );
 }
